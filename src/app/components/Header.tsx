a892aca32a60a223861c55aed060b3b696cb21fc
cyh <872888565@qq.com>
2026-06-22 16:18:16 +0800

    01 to 02

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
+import { MenuList, Title } from '@/lib/constants';
+
+export default function Header() {
+  return (
+    <div className="h-16 px-10 border-b bg-white">
+      <div className="container flex items-center justify-between h-full">
+        <h1 className="text-2xl">
+          <Link href="/">{Title}</Link>
+        </h1>
+        <div className="flex justify-end space-x-4 text-sm h-1/3">
+          {MenuList.map((item, index) => (
+            <>
+              {index !== 0 && <Separator orientation="vertical" />}
+              <Link
+                key={item.href}
+                href={item.href}
+              >
+                {item.text}
+              </Link>
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const Title = 'DUYI Store';
+
+export const MenuList = [
+  { href: '/search', text: 'Search' },
+  { href: '/account', text: 'Account' },
+  { href: '/cart', text: 'Cart' },
+];
+
+export const NavList = [
+  { title: 'Categories', list: ['Clothing', 'Audio', 'Furniture'] },
+  {
+    title: 'Collections',
+    list: ['Latest Drops', 'Best Sellers', 'Limited Edition'],
+  },
+  { title: 'Code', list: ['GitHub', 'Documentation', 'Source code'] },
+];
